a6352756519cfd90dd000826da3481f06cdbab80
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-11-13 00:21:33 -0300

    fix: tenta arrumar CORS

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import routes from "./routes";
 import "express-async-errors";
-import cors from "cors"
+import cors from "cors";
 import "./database/connection.ts";
 import dotenv from "dotenv";
 
@@ -9,19 +9,16 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors())
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Credentials", true);
-	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-	);
-	next();
-});
+const corsOpts = {
+	origin: "*",
+
+	methods: ["GET", "POST"],
+
+	allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOpts));
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(process.env.PORT);
